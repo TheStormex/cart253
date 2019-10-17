3a3a8c4abe53c2fb75305644add33c16dafd8a20
TheStormex <55155047+TheStormex@users.noreply.github.com>
2019-10-17 15:46:16 -0400

    Ex5: Added second predator and parameters to control both differently

--- Exercises/exercise5/js/script.js
+++ Exercises/exercise5/js/script.js
@@ -1,12 +1,13 @@
 // Predator-Prey Simulation
-// by Pippin Barr
+// by Pippin Barr (remixed by Che Tan)
 //
 // Creates a predator and three prey (of different sizes and speeds)
 // The predator chases the prey using the arrow keys and consumes them.
 // The predator loses health over time, so must keep eating to survive.
 
-// Our predator
+// Our predators
 let tiger;
+let snowLeopard;
 
 // The three prey
 let antelope;
@@ -16,10 +17,12 @@ let bee;
 // setup()
 //
 // Sets up a canvas
-// Creates objects for the predator and three prey
+// Creates objects for the 2 predators and three prey
+// One
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tiger = new Predator(100, 100, 5, color(200, 200, 0), 40);
+  tiger = new Predator(width/2-width/3, height/2, 5, color(255, 0, 0), 40, 87, 83, 65, 68);
+  snowLeopard = new Predator(width/2+width/3, height/2, 5, color(0, 0, 255), 40, 38, 40, 37, 39);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
   zebra = new Prey(100, 100, 8, color(255, 255, 255), 60);
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
@@ -34,9 +37,11 @@ function draw() {
 
   // Handle input for the tiger
   tiger.handleInput();
+  snowLeopard.handleInput();
 
   // Move all the "animals"
   tiger.move();
+  snowLeopard.move();
   antelope.move();
   zebra.move();
   bee.move();
@@ -45,9 +50,13 @@ function draw() {
   tiger.handleEating(antelope);
   tiger.handleEating(zebra);
   tiger.handleEating(bee);
+  snowLeopard.handleEating(antelope);
+  snowLeopard.handleEating(zebra);
+  snowLeopard.handleEating(bee);
 
   // Display all the "animals"
   tiger.display();
+  snowLeopard.display();
   antelope.display();
   zebra.display();
   bee.display();

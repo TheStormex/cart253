3e130cfa4ec6c9cd7aad007ef706208b752ec179
TheStormex <55155047+TheStormex@users.noreply.github.com>
2019-10-24 15:47:57 -0400

    Ex6: typo fixed (disploy to display)

--- Exercises/exercise6/js/script.js
+++ Exercises/exercise6/js/script.js
@@ -53,6 +53,6 @@ function draw() {
   // Display all the "animals"
   tiger.display();
   antelope.display(); //////////////// FIXED
-  zebra.disploy();
+  zebra.display(); //////////////// FIXED
   bee.display(); //////////////// FIXED
 }
